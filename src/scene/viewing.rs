c7a5a80f97bbcc0d8a4afe843e1e098cf903a00f
Paul Jarrett <jarrett.paul.young@gmail.com>
2016-12-15 08:42:10 -0500

    Adds NDC -> film conversion.

--- src/scene/viewing.rs
+++ src/scene/viewing.rs
@@ -15,6 +15,10 @@ impl Film {
     pub fn to_ndc(&self, x: i32, y: i32) -> (f32, f32) {
         (x as f32 / self.width as f32, (self.height - y) as f32 / self.height as f32)
     }
+
+    pub fn from_ndc(&self, x: f32, y: f32) -> (i32, i32) {
+        (((x * (self.width as f32)).round() as i32), (((1.0 - y) * self.height as f32) as i32))
+    }
 }
 
 
@@ -43,5 +47,17 @@ mod test {
         assert_close(f.to_ndc(800, 0), (1.0, 1.0));
         assert_close(f.to_ndc(800, 600), (1.0, 0.0));
         assert_close(f.to_ndc(0, 600), (0.0, 0.0));
+        assert_close(f.to_ndc(400, 300), (0.5, 0.5));
+    }
+
+    #[test]
+    fn test_ndc_to_film() {
+        let f = Film::new(800, 600);
+        // Try back converting our previous points.
+        assert_eq!(f.from_ndc(0.0, 1.0), (0, 0));
+        assert_eq!(f.from_ndc(1.0, 1.0), (800, 0));
+        assert_eq!(f.from_ndc(1.0, 0.0), (800, 600));
+        assert_eq!(f.from_ndc(0.0, 0.0), (0, 600));
+        assert_eq!(f.from_ndc(0.5, 0.5), (400, 300));
     }
 }
